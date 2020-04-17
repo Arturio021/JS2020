250d80ae2047761a637c2706b88db2a4f33cde47
Artur <korbut.arturio@gmail.com>
2020-04-17 14:07:00 +0300

    Lesson 3, 5 new tasks

--- Lesson1/hm_1.js
+++ Lesson1/hm_1.js
@@ -1,5 +1,28 @@
-a = ("Hello World! It's JavaScript !")
+a =
+    "Написать 5 разных вариантов подсчета и вывести результат в модальном окне !";
 console.log(a);
 
-b = alert(a)
-console.log(bhi);
+b = alert(a);
+console.log(b);
+
+/* Написать 5 разных вариантов подсчета и вывести результат в модальном окне */
+
+// Plus
+var plus = 7 + 3;
+alert("7 + 3 = " + plus);
+// Minus
+var minus = 21;
+ten = minus -= 11;
+alert("21 - 11 = " + ten);
+// multiplication
+
+var mult = 2 * 5;
+alert("2*5 = " + mult);
+
+//division
+var div = 30 / 3;
+alert("30/3 = " + div);
+
+// square root
+var sqr = Math.sqrt(100);
+alert("Square root 100 = " + sqr);
