8acefb0d2892be807a007513b10449f70d783d04
Artur <korbut.arturio@gmail.com>
2020-10-13 15:48:54 +0300

    feat: add task with new inform

--- EventLoop/script/script.js
+++ EventLoop/script/script.js
@@ -1,12 +1,32 @@
-function time(one, two) {
-    setTimeout(function() {
-        for (var i = one; i <= two; i++) {
-            console.log(i);
-        }
-    }, 1000)
+function outputNumbers(one, two) {
+    var num = two;
+    setTimeout(function time() {
+        console.log(num);
+        if (num !== one) setTimeout(time, 1000);
+        num--;
+    }, 1000);
 }
-time(1, 25)
+outputNumbers(1, 14);
 
+// function outputNumbers(one, two) {
+//     var current = one;
+//     setTimeout(function go() {
+//         console.log(current);
+//         if (current !== two) setTimeout(go, 1000);
+//         current++;
+//     }, 1000);
+// }
+
+// outputNumbers(1, 10);
+
+// function time(one, two) {
+//     setTimeout(function() {
+//         for (var i = one; i <= two; i++) {
+//             console.log(i);
+//         }
+//     }, 1000)
+// }
+// time(1, 25)
 
 // function time(one, two) {
 //     for (var i = one; i <= two; i++) {
@@ -19,9 +39,6 @@ time(1, 25)
 //setTimeout(time, 1000, 1, 20)
 // time(1, 20);
 // setTimeout(time, 1000, 1, 20)
-
-
-
 
 // function printMessage() {
 //     alert('hi');
